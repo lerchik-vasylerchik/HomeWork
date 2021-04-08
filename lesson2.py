1db0a37203b0b8511ece9dc2f8134828e4629fbe
lerchik-vasylerchik <82110693+lerchik-vasylerchik@users.noreply.github.com>
2021-04-08 20:55:54 +0300

    Lesson 3 Home-work

---
+++ lesson2.py
@@ -0,0 +1,62 @@
+# number=10
+# my_number=23
+# result=number+my_number
+# print(result)
+# qwe=123
+# asd=qwe
+# asd=qwe+3
+# #asd=4
+# print(asd)
+
+# number=10
+# print(number, type(number))
+# number=1.0
+# print(number, type(number))
+# number = '10'
+# print(number, type(number))
+
+# value=input("Input decimal number")
+# print(value)
+# result=int(value)*2
+# print(result)
+
+# value=10
+# res=float(value)
+# print(res)
+#
+# value=28.6
+# res=int(value)
+# print(res)
+#
+# value=10.34
+# res=str(value)
+# print(res)
+#
+# value="-10"
+# res=int(value)
+# print(res)
+#
+# value="-1034"
+# res=float(value)
+# print(res)
+
+# bool_value=31!=3
+# my_str="I'm a string"
+# value="12"
+# result=str(value)
+# print(bool_value, type(bool_value))
+# value=23%2==1
+# print(value)
+
+
+# value=0.000001
+# res=bool(value)
+# print(res)
+
+# value=""
+# res=bool(value)
+# print(res)
+value_1=True
+value_2=True
+res_bool=value_1 or value_2
+print(res_bool)
